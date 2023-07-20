77ccda35f7146a46f6215272694f6d24483006e9
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2023-07-20 13:04:14 -0500

    add/update project

--- src/banco/app/Aplicacion.java
+++ src/banco/app/Aplicacion.java
@@ -3,32 +3,40 @@ package banco.app;
 import banco.dominio.Banco;
 import banco.dominio.CuentaBancaria;
 import banco.dominio.Persona;
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Aplicacion {
 
     public static void main(String[] args) {
 
+        //instancia de bancos
         Banco bancolombia = new Banco("123897", "Bancolombia S.A");
-// instancia de usuarios
-        Persona emmanuelJose = new Persona(1234,"Emmanuel Jose");
-        Persona alejandro = new Persona(321564,"alejandro");
-// instancia de cuentas
-        CuentaBancaria miCuenta = new CuentaBancaria( bancolombia,"Ahorros",emmanuelJose,"9875651",350000);
-        CuentaBancaria cuentaAlejandro = new CuentaBancaria(bancolombia,"Ahorros", alejandro, "97898",985000);
 
-        miCuenta.retirar(300000);
+        // instancia de usuarios
+        Persona emmanuelJose = new Persona(1234, "Emmanuel Jose");
+        Persona alejandro = new Persona(321564, "Alejandro");
 
+        // instancia de cuentas
+        CuentaBancaria miCuenta = new CuentaBancaria(bancolombia, "Ahorros", emmanuelJose, "9875651", 350000);
+        CuentaBancaria cuentaAlejandro = new CuentaBancaria(bancolombia, "Ahorros", alejandro, "97898", 985000);
+
+    //hicimos un retiro antes de transferir a la cuenta
+        System.out.println("Se hizo retiro de la cuenta: "+ miCuenta.titular.nombre +" con saldo: "+ miCuenta.saldo );
+        miCuenta.retirar(3000);
+
+    //consultamos saldos actuales
         System.out.println(miCuenta.consultarSaldo());
         System.out.println(cuentaAlejandro.consultarSaldo());
 
-        boolean resultadoTransferencia = miCuenta.transferir(216000, cuentaAlejandro);
+//se hace transferencia por el valor definido a la cuenta destino
+        System.out.println("Se realiza una transferencia de la cuenta: "
+                + miCuenta.titular.nombre + " a la cuenta: "
+                + cuentaAlejandro.titular.nombre);
 
-        System.out.println(resultadoTransferencia);
+        boolean resultadoTransferencia = miCuenta.transferir(216000, cuentaAlejandro);
+    //se ha aplicado condicional ternario
+        System.out.println(resultadoTransferencia ? "Se a transferido correctamente" : "Fondos insuficientes");
+
+    //consultamos saldos de nuevo
         System.out.println(miCuenta.consultarSaldo());
         System.out.println(cuentaAlejandro.consultarSaldo());
 
---
+++ src/complejidadA.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class complejidadA {
+//complejidad de algoritmos
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int casos = in.nextInt();
+
+            //int casos = Integer.parseInt(System.console().readLine());
+            for(int i = 0; i < casos; i++) {
+                int prueba = in.nextInt();
+                //int prueba = Integer.parseInt(System.console().readLine());
+                String respuesta = "";
+                for (int j = 9; j > 0; j--){
+                    if(prueba >= j){
+                        respuesta = j + respuesta;
+                        prueba -= j;
+                    }
+                }
+                System.out.println(respuesta);
+            }
+        }
+
+}
